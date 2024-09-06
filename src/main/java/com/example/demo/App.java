6df12dcc744c20cbb3af91c395c9bc97d9201b29
daniilchik <dprizva55@gmail.com>
2024-09-06 20:20:45 +0300

    fix: docker

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -9,8 +9,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EntityScan(basePackages = "com.example.demo.*")
 @EnableSwagger2
 public class App {
-
+    // todo: DTO, Docker, Tests, AWS
     public static void main(String[] args) {
+        System.out.println(System.getenv());
         SpringApplication.run(App.class, args);
     }
 
